b30a7de08197bf5bbbc9382e029c4897f3d4ecaa
cacodcar <59067790+cacodcar@users.noreply.github.com>
2024-09-26 14:32:39 -0500

    param tests

--- tests/element_test.py
+++ tests/element_test.py
@@ -13,12 +13,20 @@ def prg():
 
 
 @pytest.fixture
+def s0():
+    """|s0| = 1"""
+    return S(0)
+
+
+@pytest.fixture
 def s1():
+    """|s1| = 3"""
     return S('a', 'b', 'c')
 
 
 @pytest.fixture
 def s2():
+    """|s2| = 4"""
     return S('a', 'd', 'e', 'f')
 
 
@@ -28,36 +36,6 @@ def test_set(s1, s2):
     assert (s1 | s2) == S('a', 'b', 'c', 'd', 'e', 'f')
     assert (s1 ^ s2) == S('b', 'c', 'd', 'e', 'f')
     assert (s1 - s2) == S('b', 'c')
-
-
-@pytest.fixture
-def p0():
-    """no index + scalar parameter"""
-    return P(_=5)
-
-
-@pytest.fixture
-def p1():
-    """no index + vector parameter"""
-    return P(_=[4, 5, 6])
-
-
-@pytest.fixture
-def p2(s1):
-    """index + scalar parameter"""
-    return P(s1, _=5)
-
-
-@pytest.fixture
-def p3(s2):
-    """index + vector parameter"""
-    return P(s2, _=[4, 5, 6, 7])
-
-
-@pytest.fixture
-def p4(s1, s2):
-    """indices + vector parameter"""
-    return P(s1, s2, _=list(range(12)))
 
 
 @pytest.fixture
@@ -82,48 +60,41 @@ def prgp2(prg, s1):
 
 
 @pytest.fixture
-def prgp3(prg, s2):
+def prgp3(prg, s0, s1):
     """index + vector parameter"""
-    prg.p3 = P(s2, _=[4, 5, 6, 7])
+    prg.p3 = P(s0, s1, _=[4, 5, 6])
     return prg.p3
 
 
-@pytest.fixture
-def prgp4(prg, s1, s2):
-    """indices + vector parameter"""
-    prg.p4 = P(s1, s2, _=list(range(12)))
-    return prg.p4
-
-
-def test_params(p0, p1, p2, p3, p4):
-
-    assert p0._ == 5
-    assert p1._ == [4, 5, 6]
-    assert p2._ == [5, 5, 5]
-    assert p3._ == [4, 5, 6, 7]
-    assert p4._ == list(range(12))
-
-
-def test_prgparams(prgp0, prgp1, prgp2, s1, s2, prgp4):
-
+def test_prgparams(s0, s1, prgp0, prgp1, prgp2, prgp3):
     assert prgp0._ == 5
+    assert prgp0.index == ()
     assert prgp1._ == [P(_=4), P(_=5), P(_=6)]
     assert prgp2._ == [P('a', _=5), P('b', _=5), P('c', _=5)]
+    assert prgp2.index[0] == s1
+    assert prgp3._ == [P(_=[4]), P(_=[5]), P(_=[6])]
+    assert prgp3._ == [P(s0, _=4), P(s0, _=[5]), P(s0, _=[6])]
+    assert prgp3.idx == [(0, 'a'), (0, 'b'), (0, 'c')]
+    assert prgp3._[0].index == (0, 'a')
+    assert prgp3._[1].index == (0, 'b')
+    assert prgp3._[2].index == (0, 'c')
 
 
-# @pytest.fixture
-# def v():
-#     """empty variable"""
-#     return V()
+@pytest.fixture
+def prgv1(prg, s1):
+    """indexed variable"""
+    prg.v1 = V(s1)
+    return prg.v1
 
 
-# @pytest.fixture
-# def v1(s1):
-#     """variable"""
-#     return V(s1)
+@pytest.fixture
+def prgv2(prg, s0, s1):
+    """multi-indexed variable"""
+    prg.v2 = V(s0, s1)
+    return prg.v2
 
 
-# @pytest.fixture
-# def v2(s2):
-#     """variable"""
-#     return V(s2)
+# def test_vars(prgv1, prgv2):
+
+#     assert prgv1._ == [V('a'), V('b'), V('c')]
+#     assert prgv2._ == [V(0, 'a'), V(0, 'b'), V(0, 'c')]
